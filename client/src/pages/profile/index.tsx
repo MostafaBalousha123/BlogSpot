b14e54c2cded5efb5008eedbd88f9459651d4e62
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-12-05 19:20:17 +0200

    create taps component in profile page contains two components [ Blogs, Photos ]

--- client/src/pages/profile/index.tsx
+++ client/src/pages/profile/index.tsx
@@ -1,12 +1,47 @@
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import ProfileHeader from '../../components/ProfileHeader'
+import BasicTabs from '../../components/Taps'
+import ApiService from '../../services/apiServices'
+import { IBlogs } from '../../interfaces/IBlogs'
 
 import './style.css'
 
-export const Profile:FC = () => (
-  <div>
-    <Navbar />
-    <ProfileHeader />
-  </div>
-)
+const initBlog = {
+  id: 0,
+  title: '',
+  content: '',
+  description: '',
+  image: '',
+  userId: 0,
+  createdAt: '',
+  updatedAt: '',
+  user: {
+    username: '',
+    profileImg: '',
+  },
+}
+
+export const Profile:FC = () => {
+  const [blogs, setBlogs] = useState<IBlogs[]>([initBlog])
+  const userId = useParams()
+  useEffect(() => {
+    (async () => {
+      const result = await ApiService.get('/api/v1/blogs', {
+        params: {
+          userId: userId.id,
+        },
+      })
+      if (result.status === 200) setBlogs(result.data)
+    })()
+  }, [])
+
+  return (
+    <div>
+      <Navbar />
+      <ProfileHeader />
+      <BasicTabs blogs={blogs} />
+    </div>
+  )
+}
