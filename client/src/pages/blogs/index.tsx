c7e352e1754189b754c4374dfe3e355accd68a93
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-12-05 19:16:23 +0200

    Reorganization handle get all blogs requset to reuse BlogsContainer component in profile page

--- client/src/pages/blogs/index.tsx
+++ client/src/pages/blogs/index.tsx
@@ -1,10 +1,37 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import BlogsContainer from '../../components/BlogsContainer'
 import Navbar from '../../components/Navbar'
+import ApiService from '../../services/apiServices'
+import { IBlogs } from '../../interfaces/IBlogs'
 
-export const Blogs:FC = () => (
-  <div>
-    <Navbar />
-    <BlogsContainer />
-  </div>
-)
+const initBlog = {
+  id: 0,
+  title: '',
+  content: '',
+  description: '',
+  image: '',
+  userId: 0,
+  createdAt: '',
+  updatedAt: '',
+  user: {
+    username: '',
+    profileImg: '',
+  },
+}
+
+export const Blogs:FC = () => {
+  const [blogs, setBlogs] = useState<IBlogs[]>([initBlog])
+  useEffect(() => {
+    (async () => {
+      const result = await ApiService.get('/api/v1/blogs')
+      if (result.status === 200) setBlogs(result.data)
+    })()
+  }, [])
+
+  return (
+    <div>
+      <Navbar />
+      <BlogsContainer blogs={blogs} />
+    </div>
+  )
+}
