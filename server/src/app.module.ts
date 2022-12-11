197817f8e5e63aea885eefce109f786d485a49ab
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-12-11 16:53:41 +0200

    serve static file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -15,7 +15,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'client', 'public'),
+      rootPath: join(__dirname, '..', '..', 'client', 'build'),
     }),
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Request, Response } from 'express';
 import { json } from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +21,12 @@ async function bootstrap() {
 
   app.enableCors({ origin: [Config.get('CLIENT')] });
   app.setGlobalPrefix('/api/v1');
+
+  if (Config.get('NODE_ENV') === 'prod') {
+    this.app.get('*', (req: Request, res: Response) => {
+      res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+    });
+  }
   await app.listen(port);
   console.log('server is running', port);
 }
