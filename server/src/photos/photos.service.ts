9d959a972e2ce38c21fdd97e3ecffd3a6be21570
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-12-12 19:17:59 +0200

    add orderBy to  query find all photos and blogs

--- server/src/photos/photos.service.ts
+++ server/src/photos/photos.service.ts
@@ -22,6 +22,7 @@ export class PhotosService {
     return await this.photoRepository.findAll({
       include: { model: User, attributes: ['username', 'profileImg'] },
       where: whereObj,
+      order: [['createdAt', 'DESC']],
     });
   }
 
